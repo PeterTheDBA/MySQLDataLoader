82fe16e4fc9a356452efe3bb84197b0e52980ef4
Peter <peter.t.sylvester@gmail.com>
2015-05-27 22:53:25 -0400

    Added column name list property to table class.  Created main, main_table, table, and table_column level menu methods.

--- Menu.py
+++ Menu.py
@@ -4,9 +4,9 @@ class Menu:
 
 	def __init__(self, schema):
 		self.schema = schema
-		self.table_menu_option = ["Adjust rows to be created", "Done"]
+		self.table_menu_option = ["Adjust rows to be created", "Adjust column properties"]
 
-	def menu_picker(self, menu_list):
+	def list_picker(self, menu_list):
 		validation_required = True
 		menu_number = 1
 		for choice in menu_list:
@@ -19,30 +19,64 @@ class Menu:
 		while validation_required:
 			if str(user_selection).isdigit():
 				user_selection = int(user_selection)
-				if user_selection in range(1, max_val+1):
+				if user_selection >= min_val and user_selection <= max_val:
 					validation_required = False
 				else:
 					print "Selection much be in the appropriate range."
 			else:
-				print "Selection must be an integer."
+				print "Selection must be a positive integer."
 			if validation_required:
-				user_selection = raw_input("Please enter selection: [%s-%s] " % (min_val, max_val))
+				user_selection = raw_input("Please enter selection [%s-%s]: " % (min_val, max_val))
 		return user_selection - 1
 	
+	def int_picker(self, prompt):
+		user_input = raw_input(prompt)
+		while user_input.isdigit() == False:
+			print "Selection must be a positive integer."
+			user_input = raw_input(prompt)
+		return int(user_input)
+	
+	def table_column_menu(self, table_index):
+		print "Please select what column you would like to adjust."
+		column_index = self.list_picker(self.schema.tables[table_index].column_list)
+		while column_index <= len(self.schema.tables[table_index].column_list) - 1:
+			print "Please select what column you would like to adjust."
+			column_index = self.list_picker(self.schema.tables[table_index].column_list)		
+		
+	def table_menu(self, table_index):
+		print "TABLE: " + self.schema.tables[table_index].table_name
+		print "What table value would you like to adjust?"
+		user_selection_index = self.list_picker(self.table_menu_option)
+		while user_selection_index <= len(self.table_menu_option) - 1:
+			if self.table_menu_option[user_selection_index] == "Adjust rows to be created":
+				self.schema.tables[table_index].rows_to_generate = self.int_picker("How many records should be created in this table?: ")
+				print "Set to " + str(self.schema.tables[table_index].rows_to_generate)
+			elif self.table_menu_option[user_selection_index] == "Adjust column properties":
+				self.table_column_menu(table_index)
+			print "TABLE: " + self.schema.tables[table_index].table_name
+			print "What table value would you like to adjust?"
+			user_selection_index = self.list_picker(self.table_menu_option)
+			
+	
 	def main_menu(self):
-		user_selection_index = None
+		print "Please select what table you would like to adjust."
+		table_index = self.list_picker(self.schema.table_list)
+		while table_index <= len(self.schema.table_list) - 1:
+			self.table_menu(table_index)
+			print "Please select what table you would like to adjust."
+			table_index = self.list_picker(self.schema.table_list)			
+		print "Menu Complete"
+		
+
+	#def main_menu(self):
+		#user_selection_index = None
 		#table_menu_continue = raw_input("Would you like to change any of these properties for any table or column? [y/n]: ")
 		#TODO: input validation
 		#TODO: create y_n picker function so to not dup code
 		#validate that there are any tables to use before loading prompt
 		#table_menu_continue = 'y'
 		#while table_menu_continue in ['Y', 'y']:
-		print "Please select what table you would like to adjust."
-		table_index = self.menu_picker(self.schema.table_list)
-		if table_index <= len(self.schema.table_list) - 1:
-			print "What value would you like to adjust?"
-		else:
-			print "Menu Complete"
+
 		#print "What value would you like to adjust?"
 		#table_menu_selection_index = self.menu_picker(self.table_menu_option)-1
 		#if table_menu_selection_index == 0:
